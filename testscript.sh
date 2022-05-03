0a0504525a22701935dc4d3d02bbdd8ec4962a25
Roy Sam <rsam@m-c02cjtuemd6m.paloaltonetworks.local>
2022-05-03 09:27:23 +0800

    3-May update

--- testscript.sh
+++ testscript.sh
@@ -2,5 +2,4 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "This was added"
-echo "This is Prisma Cloud enabled"
+echo "Prisma Cloud -- CI Testing --"
